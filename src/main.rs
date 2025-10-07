24d34f28c8f8bc82b969e038f1e57cfc1c32d37f
Akash <akash@macbookpro.lan>
2025-10-07 01:24:06 -0700

    Setting up basic window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::thread::sleep;
+use std::time::Duration;
+use crate::window::Chip8Window;
+
+mod window;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window = Chip8Window::new();
+
+    while window.should_run() {
+        sleep(Duration::from_millis(10));
+        window.update();
+    }
 }
---
+++ src/window.rs
@@ -0,0 +1,42 @@
+use minifb::{Scale, Window, WindowOptions};
+
+const WIDTH: usize = 64;
+const HEIGHT: usize = 32;
+
+pub struct Chip8Window {
+    pub buffer: [u32; WIDTH * HEIGHT],
+    window: Window,
+}
+
+impl Chip8Window {
+    pub fn new() -> Self {
+        let mut window = Window::new(
+            "Iron Chip",
+            WIDTH,
+            HEIGHT,
+            WindowOptions {
+                scale: Scale::X16,
+                ..Default::default()
+            }
+        ).unwrap();
+
+        // Unrestrict this so the main game loop can handle setting FPS
+        window.set_target_fps(0);
+
+        Self {
+            buffer: [0; WIDTH * HEIGHT],
+            window
+        }
+
+    }
+
+    pub fn should_run(&self) -> bool {
+        self.window.is_open()
+    }
+
+    pub fn update(&mut self) {
+        self.window
+            .update_with_buffer(&self.buffer, WIDTH, HEIGHT)
+            .unwrap();
+    }
+}
